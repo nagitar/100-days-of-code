69aa7d2fb366dcbb2fdc321fd47b15fd27df25be
Nagitar <rathorenagita26@gmail.com>
2018-07-12 10:56:41 +0530

    merge

---
+++ code/day-10/merge.py
@@ -0,0 +1,29 @@
+def mergeSort(alist):
+    print("Splitting",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i = i+1
+            else:
+                alist[k] = righthalf[j]
+                j = j+1
+                k=k+1
+                 while i < len(lefthalf):
+                     alist[k] = lefthalf[i]
+                     i=i+1
+                     k=k+1
+                     print("Merging",alist)
+                     alist = [22,21,23,65,45,87,67]
+                     mergeSort(alist)
+                     print(alist)
