cd3d24737d803463ec89fe38e77473dbd791ae6f
Nagitar <rathorenagita26@gmail.com>
2018-12-02 22:37:59 +0530

    conert the kilo to mile

---
+++ code/day-23/kilo.py
@@ -0,0 +1,6 @@
+kilometers = float(input('How many kilometers?: '))
+# conversion factor
+conv_fac = 0.621371
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))  
